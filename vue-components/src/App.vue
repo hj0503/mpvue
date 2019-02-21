807628ce6833b5c2b6ce639f6b9a5e6021ece943
hj0503 <857131753@qq.com>
2019-02-21 23:45:05 +0800

    vue component study

---
+++ vue-components/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <TitleBar title="title">
+      <h2>Hello world</h2>
+    </TitleBar>
+  </div>
+</template>
+
+<script>
+import TitleBar from './components/TitleBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TitleBar
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app{
+  width: 100%;
+}
+</style>
---
+++ vue-components/src/components/TitleBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <span class="title">{{title}}</span>
+    <span class="sub">{{subTitle}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    subTitle: {
+      type: String,
+      default: "sub-title"
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  background: #ececec;
+  padding: 0 10px;
+  box-sizing: border-box;
+}
+.sub{
+  margin-left: 10px;
+}
+</style>
